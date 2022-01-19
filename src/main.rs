4665a53938091bb4f061b6a04169bc66330f5e6b
Markus Reiter-Haas <iseratho@gmail.com>
2022-01-19 21:11:16 +0100

    First request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct ResponseNew {
+    text: String,
+    version: reqwest::Version,
+    statusCode: reqwest::StatusCode,
+}
+
+fn print_response() -> Result<(), reqwest::Error> {
+    let res = reqwest::blocking::get("https://www.example.com/")?;
+    let version: reqwest::Version = res.version();
+    // let header = res.headers();
+    let statusCode = res.status();
+    let text = match res.text() {
+        Ok(x) => x,
+        _ => panic!(),
+    };
+    let custom_response = ResponseNew {
+        text: text,
+        version: version,
+        statusCode: statusCode,
+    };
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_response();
 }
