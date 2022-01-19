43fe0349917177b2d8ee2face20cab483129a892
Markus Reiter-Haas <iseratho@gmail.com>
2022-01-19 22:34:09 +0100

    Better show case example of response printing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,55 @@
-struct ResponseNew {
+use std::fmt::Display;
+
+struct CustomResponse {
     text: String,
     version: reqwest::Version,
-    statusCode: reqwest::StatusCode,
+    headers: reqwest::header::HeaderMap,
+    status_code: reqwest::StatusCode,
 }
 
-fn print_response() -> Result<(), reqwest::Error> {
-    let res = reqwest::blocking::get("https://www.example.com/")?;
+trait Shorten {
+    fn shorten(&self) -> Self;
+}
+
+impl Display for CustomResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> { 
+        let short_string = self.text.shorten();
+        // Same as:
+        // let short_string = &self.text[..50];
+        write!(f, "{:?} - {} - {:?} - {}", self.version, self.status_code, self.headers, short_string)
+    }
+}
+
+impl Shorten for String {
+    fn shorten(&self) -> String {
+        String::from(&self[..50])
+    }
+}
+
+fn get_response(url: String) -> Result<CustomResponse, reqwest::Error> {
+    let res = reqwest::blocking::get(url)?;
     let version: reqwest::Version = res.version();
-    // let header = res.headers();
-    let statusCode = res.status();
-    let text = match res.text() {
-        Ok(x) => x,
-        _ => panic!(),
-    };
-    let custom_response = ResponseNew {
-        text: text,
+    let headers = res.headers().clone();
+    let status_code = res.status();
+    let text = res.text()?;
+    let my_response = CustomResponse {
+        text: String::from(text),
         version: version,
-        statusCode: statusCode,
+        headers: headers,
+        status_code: status_code,
     };
-    Ok(())
+    Ok(my_response)
+}
+
+impl CustomResponse {
+    fn print_response(url: String) {
+        match get_response(url) {
+            Ok(x) => println!("{}", x),
+            _ => panic!(),
+        }
+    }
 }
 
 fn main() {
-    print_response();
+    CustomResponse::print_response(String::from("https://www.example.com/"))
 }
